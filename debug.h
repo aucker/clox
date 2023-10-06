c54e43b607b7d274d789092d9fa54c5af04d3190
aucker <aucker@qq.com>
2023-10-07 04:00:35 +0800

    feat: finish disassembling chunks
    Signed-off-by: aucker <aucker@qq.com>

---
+++ debug.h
@@ -0,0 +1,13 @@
+//
+// Created by aucker on 10/7/2023.
+//
+
+#ifndef CLOX_DEBUG_H
+#define CLOX_DEBUG_H
+
+#include "chunk.h"
+
+void disassembleChunk(Chunk* chunk, const char* name);
+int disassembleInstruction(Chunk* chunk, int offset);
+
+#endif//CLOX_DEBUG_H
