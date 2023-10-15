16945954fe6b353e4264882da6c0b17a5e3234a1
aucker <aucker@qq.com>
2023-10-15 14:39:34 +0800

    update: add ParseRule
    besides, I change to function order. To do that, I need to add three
    lines of code at line 133, so that it can be called, C/C++ is special
    compared with other languages, anyway.
    
    Signed-off-by: aucker <aucker@qq.com>

--- compiler.c
+++ compiler.c
@@ -30,6 +30,19 @@ typedef enum {
     PREC_PRIMARY
 } Precedence;
 
+/*
+ * C's syntax for function pointer types is so bad that I always hide it behind a typedef.
+ *
+ * ParseFn type is a simple typedef for a function type that takes no arguments and returns nothing.
+ */
+typedef void (*ParseFn)();
+
+typedef struct {
+    ParseFn prefix;
+    ParseFn infix;
+    Precedence precedence;
+} ParseRule;
+
 Parser parser;
 Chunk* compilingChunk;
 
@@ -116,9 +129,10 @@ static void endCompiler() {
     emitReturn();
 }
 
-static void parsePrecedence(Precedence precedence) {
-    // ?
-}
+
+static void expression();
+static ParseRule* getRule(TokenType type);
+static void parsePrecedence(Precedence precedence);
 
 static void binary() {
     TokenType operatorType = parser.previous.type;
@@ -166,6 +180,56 @@ static void unary() {
     }
 }
 
+ParseRule rules[] = {
+        [TOKEN_LEFT_PAREN]      = {grouping, NULL,   PREC_NONE},
+        [TOKEN_RIGHT_PAREN]     = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_LEFT_BRACE]      = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_RIGHT_BRACE]     = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_COMMA]           = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_DOT]             = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_MINUS]           = {unary,    binary, PREC_TERM},
+        [TOKEN_PLUS]            = {NULL,     binary, PREC_TERM},
+        [TOKEN_SEMICOLON]       = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_SLASH]           = {NULL,     binary, PREC_FACTOR},
+        [TOKEN_STAR]            = {NULL,     binary, PREC_FACTOR},
+        [TOKEN_BANG]            = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_BANG_EQUAL]      = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_EQUAL]           = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_EQUAL_EQUAL]     = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_GREATER]         = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_GREATER_EQUAL]   = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_LESS]            = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_LESS_EQUAL]      = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_IDENTIFIER]      = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_STRING]          = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_NUMBER]          = {number,   NULL,   PREC_NONE},
+        [TOKEN_AND]             = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_CLASS]           = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_ELSE]            = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_FALSE]           = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_FOR]             = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_FUN]             = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_IF]              = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_NIL]             = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_OR]              = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_PRINT]           = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_RETURN]          = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_SUPER]           = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_THIS]            = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_TRUE]            = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_VAR]             = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_WHILE]           = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_ERROR]           = {NULL,     NULL,   PREC_NONE},
+        [TOKEN_EOF]             = {NULL,     NULL,   PREC_NONE},
+};
+
+static void parsePrecedence(Precedence precedence) {
+    // ?
+}
+
+static ParseRule* getRule(TokenType type) {
+    return &rules[type];
+}
 
 static void grouping() {
     expression();
@@ -196,6 +260,6 @@ bool compile(const char* source, Chunk* chunk) {
     advance();
     expression();
     consume(TOKEN_EOF, "Expect end of expression.");
-    endComiler();
+    endCompiler();
     return !parser.hadError;
 }
