2341de37b44f2e5f25909e4e47711bc31b7dc98e
aucker <aucker@qq.com>
2023-10-16 10:07:20 +0800

    update: logical not and falsiness
    Signed-off-by: aucker <aucker@qq.com>

--- vm.c
+++ vm.c
@@ -55,6 +55,10 @@ static Value peek(int distance) {
     return vm.stackTop[-1 - distance];
 }
 
+static bool isFalsey(Value value) {
+    return IS_NIL(value) || (IS_BOOL(value) && !AS_BOOL(value));
+}
+
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
@@ -127,6 +131,9 @@ static InterpretResult run() {
                 BINARY_OP(NUMBER_VAL, /);
                 break;
                 //                case OP_NEGATE:   push(-pop()); break;
+            case OP_NOT:
+                push(BOOL_VAL(isFalsey(pop())));
+                break;
             case OP_NEGATE:
                 if (!IS_NUMBER(peek(0))) {
                     runtimeError("Operand must be a number.");
