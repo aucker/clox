10883d9cf461b01cf0d46b41eca3d4f0068313fa
aucker <aucker@qq.com>
2023-10-27 01:37:54 +0800

    update: upvalue objects,

--- memory.c
+++ memory.c
@@ -52,6 +52,9 @@ static void freeObject(Obj* object) {
             FREE(ObjString, object);
             break;
         }
+        case OBJ_UPVALUE:
+            FREE(ObjUpvalue, object);
+            break;
     }
 }
 
