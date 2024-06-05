941d91b5e625eed22e598b4c51bca54d4b6e9197
aucker <aucker@qq.com>
2024-06-06 00:07:32 +0800

    arithmetic calculator: binary ops

--- src/vm.c
+++ src/vm.c
@@ -18,6 +18,12 @@ void freeVM() {}
 static InterpretResult run() {
 #define READ_BYTE() (*vm.ip++)
 #define READ_CONSTANT() (vm.chunk->constants.values[READ_BYTE()])
+#define BINARY_OP(op) \
+  do {                \
+    double b = pop(); \
+    double a = pop(); \
+    push(a op b);     \
+  } while (false)
 
   for (;;) {
 #ifdef DEBUG_TRACE_EXECUTION
@@ -37,6 +43,18 @@ static InterpretResult run() {
         push(constant);
         break;
       }
+      case OP_ADD:
+        BINARY_OP(+);
+        break;
+      case OP_SUBTRACT:
+        BINARY_OP(-);
+        break;
+      case OP_MULTIPLY:
+        BINARY_OP(*);
+        break;
+      case OP_DIVIDE:
+        BINARY_OP(/);
+        break;
       case OP_NEGATE:
         push(-pop());
         break;
@@ -51,6 +69,7 @@ static InterpretResult run() {
 
 #undef READ_BYTE
 #undef READ_CONSTANT  // fastidious, C punish sloppy user, preprocessor doubly
+#undef BINARY_OP
 }
 
 InterpretResult interpret(Chunk* chunk) {
