96a74c662f7837d1817ae45dfa1abebde5c14b15
aucker <aucker@qq.com>
2024-06-06 16:05:05 +0800

    Scanning: Interpreter: compilation pipline

--- src/vm.c
+++ src/vm.c
@@ -4,6 +4,7 @@
 
 #include "chunk.h"
 #include "common.h"
+#include "compiler.h"
 #include "debug.h"
 #include "value.h"
 
@@ -72,10 +73,9 @@ static InterpretResult run() {
 #undef BINARY_OP
 }
 
-InterpretResult interpret(Chunk* chunk) {
-  vm.chunk = chunk;
-  vm.ip = vm.chunk->code;
-  return run();
+InterpretResult interpret(const char* source) {
+  compile(source);
+  return INTERPRET_OK;
 }
 
 void push(Value value) {
